5df93cea633249021878643bf8f1272f153300c5
alirezaarv <alireza.arv.mail@gmail.com>
2019-10-04 08:52:19 +0330

    models -> model

---
+++ api/utils/mongo.js
@@ -0,0 +1,23 @@
+const mongo = require("mongoose");
+const fs = require("fs");
+const path = require("path");
+
+const database = "karjo";
+
+mongo.set("useCreateIndex", true);
+
+mongo.connect(`mongodb://mongo:27017/${database}`, {
+  poolSize: 10,
+  useNewUrlParser: true
+});
+
+const models = fs.readdirSync(path.join(__dirname, "..", "model"));
+
+models.forEach(m => {
+  const [modelName] = m.split(".");
+  const schemaPath = path.join(__dirname, "..", "model", modelName);
+  const schema = require(schemaPath);
+  mongo.model(modelName, schema);
+});
+
+module.exports = mongo;
