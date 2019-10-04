f03b21063e7756aa776e62fde6d8c0d51fbd30d2
alirezaarv <alireza.arv.mail@gmail.com>
2019-10-04 11:38:57 +0330

    get jobs updated

--- api/getJobs.js
+++ api/getJobs.js
@@ -3,10 +3,14 @@ const mongo = require("./utils/mongo");
 const Job = mongo.model("Job");
 
 const getJobs = async (ctx, params) => {
+  const query = {};
+  if (ctx && ctx.user && ctx.user.skillIds) {
+    // update query based on user skillIds
+  }
   const jobs = await Job.find();
   return { jobs };
 };
 
 module.exports = {
-  func: getJobs,
+  func: getJobs
 };
